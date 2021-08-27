251ce5a4701e6c4cabf59d8fd998b8bc2ebb03fc
Viacheslav Bessonov <viacheslav.bessonov@hilbertteam.com>
2021-08-28 00:32:06 +0500

    Enhance DateParser and DateTimeParser to be able to parse ISO 8601 date & time values

--- tests/webpub_manifest_parser/core/test_parsers.py
+++ tests/webpub_manifest_parser/core/test_parsers.py
@@ -1,5 +1,9 @@
+import datetime
+from abc import ABCMeta
 from unittest import TestCase
 
+import pytz
+import six
 from nose.tools import assert_raises
 from parameterized import parameterized
 
@@ -11,44 +15,209 @@ from webpub_manifest_parser.core.ast import (
 from webpub_manifest_parser.core.parsers import (
     AnyOfParser,
     ArrayParser,
+    DateParser,
+    DateTimeParser,
     NumberParser,
     StringParser,
     TypeParser,
     URIParser,
+    ValueParser,
     ValueParserError,
     find_parser,
 )
 
 
-class NumberParserTest(TestCase):
-    @parameterized.expand(
-        [("correct_number", "123"), ("incorrect_number", "abc", ValueParserError)]
-    )
-    def test(self, _, value, expected_error_class=None):
-        validator = NumberParser()
+@six.add_metaclass(ABCMeta)
+class ParserTest(object):
+    PARSER_CLASS = ValueParser
 
-        if expected_error_class:
-            with assert_raises(expected_error_class):
-                validator.parse(value)
+    def _create_parser(self):
+        return self.PARSER_CLASS()
+
+    def test(self, _, value, expected_result, expected_error=None):
+        """Ensure that the parser returns the expected result or raises the expected exception."""
+        parser = self._create_parser()
+
+        if expected_error:
+            with assert_raises(expected_error.__class__) as error_context:
+                parser.parse(value)
+
+            self.assertEqual(
+                error_context.exception.error_message, expected_error.error_message
+            )
         else:
-            validator.parse(value)
+            result = parser.parse(value)
+
+            self.assertEqual(expected_result, result)
 
 
-class URIParserTest(TestCase):
+class NumberParserTest(ParserTest, TestCase):
+    PARSER_CLASS = NumberParser
+
     @parameterized.expand(
         [
-            ("correct_uri", "http://example.com"),
-            ("incorrect_uri", "123", ValueParserError),
+            ("correct_number", "123", 123),
+            (
+                "incorrect_number",
+                "abc",
+                None,
+                ValueParserError("abc", "could not convert string to float: 'abc'")
+                if six.PY3
+                else ValueParserError("abc", "could not convert string to float: abc"),
+            ),
         ]
     )
-    def test(self, _, value, expected_error_class=None):
-        validator = URIParser()
+    def test(self, _, value, expected_result, expected_error_class=None):
+        super(NumberParserTest, self).test(
+            _, value, expected_result, expected_error_class
+        )
 
-        if expected_error_class:
-            with assert_raises(expected_error_class):
-                validator.parse(value)
-        else:
-            validator.parse(value)
+
+class URIParserTest(ParserTest, TestCase):
+    PARSER_CLASS = URIParser
+
+    @parameterized.expand(
+        [
+            ("correct_uri", "http://example.com", "http://example.com"),
+            (
+                "incorrect_uri",
+                "123",
+                None,
+                ValueParserError("123", "'123' is not a 'uri'"),
+            ),
+        ]
+    )
+    def test(self, _, value, expected_result, expected_error_class=None):
+        super(URIParserTest, self).test(_, value, expected_result, expected_error_class)
+
+
+class DateParserTest(ParserTest, TestCase):
+    PARSER_CLASS = DateParser
+
+    @parameterized.expand(
+        [
+            (
+                "yyyy",
+                "2020",
+                datetime.datetime(2020, 1, 1, 0, 0),
+            ),
+            (
+                "yyyy",
+                2020,
+                None,
+                ValueParserError("2020", "Value '2020' must be a string"),
+            ),
+            (
+                "yyyy-mm",
+                "2020-01",
+                datetime.datetime(2020, 1, 1),
+            ),
+            ("yyyy-mm-dd", "2020-01-01", datetime.datetime(2020, 1, 1)),
+            (
+                "incorrect_date",
+                "2020-01-0",
+                None,
+                ValueParserError(
+                    "2020-01-0", "Value '2020-01-0' is not a correct date"
+                ),
+            ),
+            (
+                "yyyy-mm-ddThh:mm:ss",
+                "2020-01-01T10:10:10",
+                None,
+                ValueParserError(
+                    "2020-01-01T10:10:10",
+                    "Value '2020-01-01T10:10:10' is not a correct date",
+                ),
+            ),
+            (
+                "yyyy-mm-ddThh:mm:ss",
+                "2020-01-01 10:10:10",
+                None,
+                ValueParserError(
+                    "2020-01-01 10:10:10",
+                    "Value '2020-01-01 10:10:10' is not a correct date",
+                ),
+            ),
+            (
+                "yyyy-mm-ddThh:mm:ss",
+                "2020-01-01 10:10:10-03:00",
+                None,
+                ValueParserError(
+                    "2020-01-01 10:10:10-03:00",
+                    "Value '2020-01-01 10:10:10-03:00' is not a correct date",
+                ),
+            ),
+        ]
+    )
+    def test(self, _, value, expected_result, expected_error_class=None):
+        super(DateParserTest, self).test(
+            _, value, expected_result, expected_error_class
+        )
+
+
+class DateTimeParserTest(ParserTest, TestCase):
+    PARSER_CLASS = DateTimeParser
+
+    @parameterized.expand(
+        [
+            (
+                "yyyy",
+                "2020",
+                datetime.datetime(2020, 1, 1),
+            ),
+            (
+                "yyyy",
+                2020,
+                None,
+                ValueParserError("2020", "Value '2020' must be a string"),
+            ),
+            (
+                "yyyy-mm",
+                "2020-01",
+                datetime.datetime(2020, 1, 1),
+            ),
+            ("yyyy-mm-dd", "2020-01-01", datetime.datetime(2020, 1, 1)),
+            (
+                "incorrect_date",
+                "2020-01-0",
+                None,
+                ValueParserError(
+                    "2020-01-0",
+                    "Value '2020-01-0' is not a correct date & time value: it does not comply with ISO 8601 date & time formatting rules",
+                ),
+            ),
+            (
+                "yyyy-mm-ddThh:mm:ss",
+                "2020-01-01T10:10:10",
+                datetime.datetime(2020, 1, 1, 10, 10, 10),
+            ),
+            (
+                "yyyy-mm-ddThh:mm:ss",
+                "2020-01-01 10:10:10",
+                datetime.datetime(2020, 1, 1, 10, 10, 10),
+            ),
+            (
+                "yyyy-mm-ddThh:mm:ss",
+                "2020-01-01 10:10:10-03:00",
+                datetime.datetime(
+                    2020,
+                    1,
+                    1,
+                    10,
+                    10,
+                    10,
+                    tzinfo=pytz.FixedOffset(
+                        datetime.timedelta(hours=-3).total_seconds() / 60
+                    ),
+                ),
+            ),
+        ]
+    )
+    def test(self, _, value, expected_result, expected_error_class=None):
+        super(DateTimeParserTest, self).test(
+            _, value, expected_result, expected_error_class
+        )
 
 
 class FunctionsTest(TestCase):
